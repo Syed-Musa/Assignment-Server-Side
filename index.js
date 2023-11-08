e78f3da08fdaff9775ab322eb8d3cdc0464306e9
Syed Musa <musabinsyed@gmail.com>
2023-11-08 20:50:58 +0600

    added a posted section

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jv3edzu.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,6 +26,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = async(req, res, next) =>{
+  console.log('called', req.host, req.originalUrl)
+  next();
+};
+
+const verifyToken = async(req, res, next) =>{
+  const token = req.cookies?.token;
+  // console.log('token verified', token)
+  if(!token){
+    return res.status(401).send({message: 'unaothorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unaothorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,24 +55,29 @@ async function run() {
     const jobsCollection = client.db('onlineMarket').collection('jobs');
     const companyCollection = client.db('onlineMarket').collection('companies');
 
-    app.post('/jwt', async(req, res)=>{
+    app.post('/jwt', logger, async(req, res)=>{
       const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
       res
       .cookie('token', token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none'
       })
       .send({success: true});
     })
 
-    app.get('/jobs', async(req, res)=>{
+    app.get('/jobs', logger, async(req, res)=>{
       const cursor = jobsCollection.find();
+      console.log('valid in the token', req.user)
+      console.log('from valid token', req.user);
       const result = await cursor.toArray();
+      // const result = await jobsCollection.find(query).toArray();
       res.send(result);
-    });
+    });    
 
     app.get('/jobs/:id', async(req, res)=>{
       const id = req.params.id;
@@ -59,16 +89,44 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/postedjob', async(req, res)=>{
+    app.post('/jobs', async(req, res)=>{
+      const newJobs = req.body;
+      console.log(newJobs);
+      const result = await jobsCollection.insertOne(newJobs);
+      res.send(result);
+    });
+
+    app.get('/postedjob', logger, async(req, res)=>{
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.post('/jobs', async(req, res)=>{
-      const newJobs = req.body;
-      console.log(newJobs);
-      const result = await jobsCollection.insertOne(newJobs);
+    app.post('/postedjob', async(req, res)=>{
+      const postedData = req.body;
+      console.log(postedData);
+      const result = await jobsCollection.insertOne(postedData);
+      res.send(result);
+    });
+
+    app.put('/postedjob/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await jobsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete('/postedjob/:id', async(req ,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.deleteOne(query);
       res.send(result);
     })
 
